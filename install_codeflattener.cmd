1fb5f7e26ade397eb268dcbab0ff0aca094b6d9f
Will Morris <willmorris103@gmail.com>
2024-08-20 14:32:08 -0500

    .cmd installer

---
+++ install_codeflattener.cmd
@@ -0,0 +1,22 @@
+@echo off
+:: Install script for CodeFlattener (CMD)
+
+set "installDir=%USERPROFILE%\CodeFlattener"
+mkdir "%installDir%" 2>nul
+
+:: Download files
+powershell -Command "& {Invoke-WebRequest -Uri 'https://github.com/Willmo103/FlattenCodeBase/releases/download/v2.2.0/CodeFlattener.exe' -OutFile '%installDir%\CodeFlattener.exe'}"
+powershell -Command "& {Invoke-WebRequest -Uri 'https://github.com/Willmo103/FlattenCodeBase/releases/download/v2.2.0/appsettings.json' -OutFile '%installDir%\appsettings.json'}"
+powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Willmo103/CodeFlattener_SourceControl/main/setup_flattener_vcs.py' -OutFile '%installDir%\Setup_flattener_vcs.py'}"
+
+:: Create a batch file to run the Python script
+echo @echo off > "%installDir%\fltn.bat"
+echo python "%installDir%\Setup_flattener_vcs.py" %%* >> "%installDir%\fltn.bat"
+
+:: Add the installation directory to the user's PATH
+setx PATH "%PATH%;%installDir%"
+
+echo Installation completed. CodeFlattener is installed in: %installDir%
+echo The 'fltn' command has been added to your PATH.
+echo Please restart your command prompt to use the new command.
+echo Usage: fltn [path_to_codebase]
